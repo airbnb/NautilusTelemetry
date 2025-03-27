8107960eab2f736c9455c2b573f5c6e4cac54499
Ladd Van Tol <ladd.vantol@airbnb.com>
2025-03-27 15:14:47 -0700

    Span propagation changes

--- Tests/NautilusTelemetryTests/TraceExporterTests.swift
+++ Tests/NautilusTelemetryTests/TraceExporterTests.swift
@@ -37,15 +37,16 @@ final class TraceExporterTests: XCTestCase {
 	let remoteCollectorEndpoint = "https://FILL_IN_HERE/v1/traces"
 
 	// Setup for a local Jaeger instance run with instructions from: https://www.jaegertracing.io/docs/2.4/getting-started/
-	//	docker run --rm --name jaeger \
-	//	  -p 16686:16686 \
-	//	  -p 4317:4317 \
-	//	  -p 4318:4318 \
-	//	  -p 5778:5778 \
-	//	  -p 9411:9411 \
-	//	  -v /path/to/local/config.yaml:/jaeger/config.yaml \
-	//	  jaegertracing/jaeger:2.4.0 \
-	//	  --config /jaeger/config.yaml
+	/*
+	 docker run --rm --name jaeger \
+	   -p 16686:16686 \
+	   -p 4317:4317 \
+	   -p 4318:4318 \
+	   -p 5778:5778 \
+	   -p 9411:9411 \
+	   jaegertracing/jaeger:2.4.0
+	 */
+	
 	let localEndpointBase = "http://localhost:4318"
 
 	enum TestError: Error {
