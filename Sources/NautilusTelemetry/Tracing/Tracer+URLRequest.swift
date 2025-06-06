5d6ab0169b2f287accfdb87abb9904b3c85addae
Jon Parise <jon.parise@airbnb.com>
2025-06-06 18:56:59 -0400

    Add optional 'template' argument to startSpan
    When this value is provided, it will be used to form the span's name and
    set at the 'url.template' span attribute.
    
    https://opentelemetry.io/docs/specs/semconv/registry/attributes/url/#url-template

--- Sources/NautilusTelemetry/Tracing/Tracer+URLRequest.swift
+++ Sources/NautilusTelemetry/Tracing/Tracer+URLRequest.swift
@@ -10,13 +10,17 @@ public extension Tracer {
 	/// Create a manually managed span to represent an URLRequest that is about to be dispatched.
 	/// - Parameters:
 	///   - for: the URLRequest. The `traceparent` header will be added if needed.
+	///   - template: optional [`url.template`](https://opentelemetry.io/docs/specs/semconv/registry/attributes/url/#url-template) value.
 	///   - attributes: optional attributes.
 	///   - baggage: Optional ``Baggage``, describing parent span. If nil, will be inferred from task/thread local baggage.
 	/// - Returns: A newly created span.
-	func startSpan(for request: inout URLRequest, attributes: TelemetryAttributes? = nil, baggage: Baggage? = nil) -> Span {
-		let name = Span.name(forRequest: request)
+	func startSpan(for request: inout URLRequest, template: String? = nil, attributes: TelemetryAttributes? = nil, baggage: Baggage? = nil) -> Span {
+		let name = Span.name(forRequest: request, target: template)
 		let span = startSpan(name: name, kind: .client, attributes: attributes, baggage: baggage)
 		span.addTraceHeadersIfSampling(&request)
+		if let template {
+			span.addAttribute("url.template", template)
+		}
 		return span
 	}
 }
