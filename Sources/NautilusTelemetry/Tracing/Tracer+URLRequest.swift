c2cf1f2764581ffc4d645120692a85eca904ca5c
Jon Parise <jon.parise@airbnb.com>
2025-06-11 14:04:46 -0400

    Improve startSpan argument names (#26)
    Remove the 'for' label name in favor of 'request'. This feels more
    natural and obvious given that its an `inout` parameter.
    
    Also, prefer 'captureHeaders' to 'headersToCapture' to communicate this
    is an action that this method will perform; that set isn't retained and
    won't be applied to any future headers that are added to the request.
    
    Lastly, we had some redundant tests for this function, so consolidate
    them.

--- Sources/NautilusTelemetry/Tracing/Tracer+URLRequest.swift
+++ Sources/NautilusTelemetry/Tracing/Tracer+URLRequest.swift
@@ -9,13 +9,13 @@ public extension Tracer {
 	
 	/// Create a manually managed span to represent an URLRequest that is about to be dispatched.
 	/// - Parameters:
-	///   - for: the URLRequest. The `traceparent` header will be added if needed.
+	///   - request: the URLRequest. The `traceparent` header will be added if needed.
 	///   - template: optional [`url.template`](https://opentelemetry.io/docs/specs/semconv/registry/attributes/url/#url-template) value.
-	///   - headersToCapture: a set of request headers to capture, or nil to capture none.
+	///   - captureHeaders: a set of request headers to capture, or nil to capture none.
 	///   - attributes: optional attributes.
 	///   - baggage: Optional ``Baggage``, describing parent span. If nil, will be inferred from task/thread local baggage.
 	/// - Returns: A newly created span.
-	func startSpan(for request: inout URLRequest, template: String? = nil, headersToCapture: Set<String>? = nil, attributes: TelemetryAttributes? = nil, baggage: Baggage? = nil) -> Span {
+	func startSpan(request: inout URLRequest, template: String? = nil, captureHeaders: Set<String>? = nil, attributes: TelemetryAttributes? = nil, baggage: Baggage? = nil) -> Span {
 		let name = Span.name(forRequest: request, target: template)
 		let span = startSpan(name: name, kind: .client, attributes: attributes, baggage: baggage)
 		span.addAttribute("http.request.method", request.httpMethod ?? "_OTHER")
@@ -33,9 +33,9 @@ public extension Tracer {
 		span.addHeaders(
 			prefix: "http.request.header",
 			headers: request.allHTTPHeaderFields,
-			headersToCapture: headersToCapture
+			headersToCapture: captureHeaders
 		)
-		span.addTraceHeadersIfSampling(&request)
+		span.addTraceHeadersIfSampling(&request, isSampling: isSampling)
 
 		return span
 	}
